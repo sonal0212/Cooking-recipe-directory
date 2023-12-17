6797aa99a39647d5a1437e9d6baaaf4403a533a1
sonal0212 <2122002sonal@gmail.com>
2023-12-17 16:28:48 +0530

    recipeList components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,14 @@
-import './Home.css'
-export default function Home(){
-    return (
-        <div>
-            <h2>Home</h2>
-        </div>
-    );
-};
+import "./Home.css";
+import { useFetch } from "../../hook/useFetch";
+import RecipeList from "../../components/RecipeList";
+
+export default function Home() {
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data}/>}
+    </div>
+  );
+}
