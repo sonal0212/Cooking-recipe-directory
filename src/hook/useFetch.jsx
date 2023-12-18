1ac675fc8d91cd57f9492b4a815a509671084224
sonal0212 <2122002sonal@gmail.com>
2023-12-18 13:52:13 +0530

    new recipe using post request

--- src/hook/useFetch.jsx
+++ src/hook/useFetch.jsx
@@ -1,43 +1,59 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
-export const useFetch = (url) => {
-  const [data, setData] = useState(null)
-  const [isPending, setIsPending] = useState(false)
-  const [error, setError] = useState(null)
+export const useFetch = (url, method = "GET") => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+  const [options, setOptions] = useState(null);
+
+  const postData = (postData) => {
+    setOptions({
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", //type of request we are sending into the POST request i.e json data
+      },
+      body: JSON.stringify(postData), //turns the javascript object into json strings ready for posting
+    });
+  };
 
   useEffect(() => {
-    const controller = new AbortController()
-    
-    const fetchData = async () => {
-      setIsPending(true)
-      
-      try {
-        const res = await fetch(url, { signal: controller.signal })
-        if(!res.ok) {
-          throw new Error(res.statusText)
-        }
-        const data = await res.json()
+    const controller = new AbortController();
 
-        setIsPending(false)
-        setData(data)
-        setError(null)
+    const fetchData = async (fetchOptions) => {
+      setIsPending(true);
+
+      try {
+        const res = await fetch(url, { ...fetchOptions, signal: controller.signal });
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        const data = await res.json();
+
+        setIsPending(false);
+        setData(data);
+        setError(null);
       } catch (err) {
         if (err.name === "AbortError") {
-          console.log("the fetch was aborted")
+          console.log("the fetch was aborted");
         } else {
-          setIsPending(false)
-          setError('Could not fetch the data')
+          setIsPending(false);
+          setError("Could not fetch the data");
         }
       }
-    }
+    };
 
-    fetchData()
+    // invoke the function
+    if (method === "GET") {
+      fetchData()
+    }
+    if (method === "POST" && options) {
+      fetchData(options)
+    }
 
     return () => {
-      controller.abort()
-    }
+      controller.abort();
+    };
+  }, [url]);
 
-  }, [url])
-
-  return { data, isPending, error }
-}
+  return { data, isPending, error, postData };
+};
--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,9 +1,99 @@
+import { useState, useRef } from "react";
+import { useFetch } from "../../hook/useFetch";
 import "./Create.css";
 
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+  const [newIngredient, setNewIngredient] = useState(""); // for each element of the ingredient
+  const [ingredients, setIngredients] = useState([]); //adding each ing. to an array
+  const ingredientInput = useRef(null);
+
+  const { postData, data, error } = useFetch(
+    "http://localhost:3000/recipes",
+    "POST"
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    postData({
+      title,
+      ingredients,
+      method,
+      cookingTime: cookingTime + "minutes",
+    });
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const ing = newIngredient.trim(); // take away any white space.
+
+    if (ing && !ingredients.includes(ing)) {
+      //removes the repeated ing.
+      setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+    }
+
+    setNewIngredient("");
+    ingredientInput.current.focus(); //focus on the input field so that the cursor automatic ally into the input feild.
+  };
+
   return (
-  <div>
-    <h2>Create</h2>
-  </div>
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Recipe Ingredients:</span>
+          <div className="ingredients">
+            <input
+              type="text"
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button onClick={handleAdd} className="btn">
+              add
+            </button>
+          </div>
+        </label>
+        <p>
+          Current ingredients:{" "}
+          {ingredients.map((i) => (
+            <em key={i}>{i}, </em>
+          ))}
+        </p>
+
+        <label>
+          <span>Recipe Method:</span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Cooking time (minutes):</span>
+          <input
+            type="number"
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+
+        <button className="btn">submit</button>
+      </form>
+    </div>
   );
 }
